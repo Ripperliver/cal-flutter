1423a46b69e0b468937abe354f126d34fa2c6af2
unknown <liverpool.55@hotmail.com>
2019-09-09 23:04:09 -0500

    flutter

---
+++ flutter_app_calculadora/android/app/src/main/kotlin/com/ripper/flutter_app_calculadora/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ripper.flutter_app_calculadora
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
